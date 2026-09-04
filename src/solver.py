f9b069e0c5c10cdda683ad0085e9ef51f17ee48c
Bhavya-Agarwal089 <f20260089@hyderabad.bits-pilani.ac.in>
2026-09-05 00:10:25 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -16,7 +16,91 @@ def count_neighbors(grid, row, col):
     alive_count = 0
     
     # TODO: Implement your neighbor-counting logic here!
+     
 
+
+               
+    if row==0 and col!= 0 and col!=(len(grid[0])-1):
+        for i in range(0,2):
+                for j in range (col-1,col+2):
+                    if i ==row and j == col :
+                       continue
+                    else:
+                        if grid[i][j] == 1:
+                            alive_count += 1
+
+
+    elif row==(len(grid)-1) and col!= 0 and col!=(len(grid[0])-1):
+            for i in range(row-1,row+1):
+                    for j in range (col-1,col+2):
+                        if i ==row and j == col :
+                           continue
+                        else:
+                            if grid[i][j] == 1:
+                                alive_count += 1
+
+    elif row!=0 and row!=(len(grid)-1) and col ==0:
+                for i in range(row-1,row+2):
+                        for j in range (col,col+2):
+                            if i ==row and j == col :
+                               continue
+                            else:
+                                if grid[i][j] == 1:
+                                    alive_count += 1
+
+    elif col==(len(grid[0])-1) and row != 0 and row != (len(grid)-1):
+                for i in range(row-1,row+2):
+                        for j in range (col-1,col+1):
+                            if i ==row and j == col :
+                               continue
+                            else:
+                                if grid[i][j] == 1:
+                                    alive_count += 1
+
+    elif row==0 and col == 0:
+                for i in range(0,2):
+                        for j in range (0,2):
+                            if i ==row and j == col :
+                               continue
+                            else:
+                                if grid[i][j] == 1:
+                                    alive_count += 1
+    elif row==0 and col == (len(grid[0])-1):
+                    for i in range(0,2):
+                            for j in range (col-1,col+1):
+                                if i ==row and j == col :
+                                   continue
+                                else:
+                                    if grid[i][j] == 1:
+                                        alive_count += 1
+
+    elif row==(len(grid)-1) and col == 0:
+                    for i in range(row-1,row+1):
+                            for j in range (col,col+2):
+                                if i ==row and j == col :
+                                   continue
+                                else:
+                                    if grid[i][j] == 1:
+                                        alive_count += 1
+
+    elif row==(len(grid)-1) and col == (len(grid[0])-1):
+                    for i in range(row-1,row+1):
+                            for j in range (col-1,col+1):
+                                if i ==row and j == col :
+                                   continue
+                                else:
+                                    if grid[i][j] == 1:
+                                        alive_count += 1
+
+    else:
+        for i in range(row-1,row+2):
+            for j in range (col-1,col+2):
+                if i ==row and j == col :
+                   continue
+                else:
+                    if grid[i][j] == 1:
+                        alive_count += 1
+                      
     return alive_count
 
 #---------------------------- TASK 2 ----------------------------
@@ -37,7 +121,7 @@ def compute_next_generation(grid):
     """
     
     rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
+    cols = len(grid[0])
     
     # Create a new blank grid of the same size, filled with 0s (dead cells)
     next_grid = [[0 for _ in range(cols)] for _ in range(rows)]
@@ -45,5 +129,18 @@ def compute_next_generation(grid):
     # TODO: Iterate through every cell in the `grid`.
     # TODO: Use your `count_neighbors` function to find out how many neighbors it has.
     # TODO: Apply the 4 Rules of Life to determine if it should be 1 (alive) or 0 (dead) in `next_grid`.
+    
 
+    for i in range(rows):
+        for j in range(cols):
+               if grid[i][j] == 1:
+                   if count_neighbors(grid,i,j) == 2 or count_neighbors(grid,i,j) == 3:
+                       next_grid[i][j] = 1
+                   else:
+                       next_grid[i][j] = 0
+               elif grid[i][j] == 0:
+                    if count_neighbors(grid,i,j) == 3:
+                        next_grid[i][j] = 1
+                    else:
+                        next_grid[i][j] = 0
     return next_grid
